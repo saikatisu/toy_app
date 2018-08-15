68d95750733f6218592bb0fe340df68b86c2d78e
My Name <saikatisu@gmail.com>
2018-08-15 21:15:26 +0900

    Make a basic User model (including secure passwords)

--- db/migrate/20180728142819_create_users.rb
+++ db/migrate/20180728142819_create_users.rb
@@ -1,10 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
+  	add_index :users, :email, unique: true
+    # create_table :users do |t|
+    #   t.string :name
+    #   t.string :email
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+
+    # end
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,45 +5,55 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:users)
+  # end
+
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
+
+  # test "should create user" do
+  #   assert_difference('User.count') do
+  #     post :create, user: { email: @user.email, name: @user.name }
+  #   end
+
+  #   assert_redirected_to user_path(assigns(:user))
+  # end
+
+  # test "should show user" do
+  #   get :show, id: @user
+  #   assert_response :success
+  # end
+
+  # test "should get edit" do
+  #   get :edit, id: @user
+  #   assert_response :success
+  # end
+
+  # test "should update user" do
+  #   patch :update, id: @user, user: { email: @user.email, name: @user.name }
+  #   assert_redirected_to user_path(assigns(:user))
+  # end
+
+  # test "should destroy user" do
+  #   assert_difference('User.count', -1) do
+  #     delete :destroy, id: @user
+  #   end
+
+  #   assert_redirected_to users_path
+  # end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @user
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, name: @user.name }
-    assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
