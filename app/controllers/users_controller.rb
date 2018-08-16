0107d395a630c0e4ae8bf3bba82a813efcc20b2b
My Name <saikatisu@gmail.com>
2018-08-16 15:09:42 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -24,16 +25,23 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params)  # 実装は終わっていないことに注意!
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
+    if @user.save
+      # Handle a successful save.
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+    else
+      render 'new'
     end
   end
 
@@ -69,6 +77,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
--- db/migrate/20180728142819_create_users.rb
+++ db/migrate/20180728142819_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-  	add_index :users, :email, unique: true
-    # create_table :users do |t|
-    #   t.string :name
-    #   t.string :email
+  	# add_index :users, :email, unique: true
+    create_table :users do |t|
+      t.string :name
+      t.string :email
 
-    #   t.timestamps null: false
+      t.timestamps null: false
 
-    # end
+    end
   end
 end
